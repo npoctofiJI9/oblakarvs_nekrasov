8c94cc6422b126e9d451d5716545ed5eaea25881
npoctofiJI9 <61415856+npoctofiJI9@users.noreply.github.com>
2022-02-08 22:17:11 +0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,7 @@ class NombresForms(forms.ModelForm):
     def clean_innum(self):
         num = self.cleaned_data.get('innum')
         if Nombres.objects.filter(innum=num-1).exists():
-            raise ValidationError('Pososi na 1 mensche uzhe est, lox')
+            raise ValidationError('na 1 mensche uzhe est')
         return num
 
     class Meta:
